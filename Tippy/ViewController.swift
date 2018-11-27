39bba6e85a0d48bfdd0ecfdd49e8aa56990f8489
kin <bigso28@bigso28s-macbook-pro.local>
2018-11-27 02:33:56 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by kin on 11/27/18.
+//  Copyright © 2018 Kin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
